298891c6699595d6c210ea05afe0c9ff6c8cc3a6
Automatik02 <pro.descombes@gmail.com>
2023-02-20 06:40:11 -0800

    Task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Return the value of the a random number
+ */
+int main(void)
+{
+	int n = 48;
+	while (n < ':')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
