52e5083cdc64b3b4c9bc802fc385a14b78c0c3d3
Automatik02 <pro.descombes@gmail.com>
2023-02-20 04:53:54 -0800

    Task 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Return the value of the a random number
+ */
+int main(void)
+{
+	int n;
+
+	n = 'a';
+	
+	while ( n <= 'z' )
+	{
+		putchar(n);
+		n++;
+	}
+	putchar ('\n');
+
+	return (0);
+}
