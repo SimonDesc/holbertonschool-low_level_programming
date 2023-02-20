e98b2aa27773f3154a1a2f1b5cbe7ce4145c56b5
Automatik02 <pro.descombes@gmail.com>
2023-02-20 05:16:00 -0800

    Task 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Return the value of the a random number
+ */
+int main(void)
+{
+	int n = 'A';
+	int i = 'a';
+	
+        while (i >= 97 && i <=122)
+        {
+                putchar(i);
+                i++;
+        }
+
+	while (n >= 65 && n <=90)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar ('\n');
+
+	return (0);
+}
