56b5eb3f6888b9da1b61310f10a2aca0f4029d1f
Automatik02 <pro.descombes@gmail.com>
2023-02-21 04:53:38 -0800

    Task 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print _islower - return 1 if lowercase 0 .
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
