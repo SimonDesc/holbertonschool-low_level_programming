12a0d0ca8eeef38b74992e61ad8869a6bbe085ce
Automatik02 <pro.descombes@gmail.com>
2023-02-21 02:56:59 -0800

    Task 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10x alphabet.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 11)
+	{
+		int j = 97;
+
+		while (j < 123)
+		{
+			_putchar(j);
+			j++;
+		}
+		i++;
+		_putchar('\n');
+	}
+}
