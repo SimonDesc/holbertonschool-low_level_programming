21e687fd13af1b6ae5d435b611de92b791a3b43e
Automatik02 <pro.descombes@gmail.com>
2023-02-21 01:18:24 -0800

    Task 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void printfunc(char a);
+
+int main(void)
+{
+	printfunc('_');
+	printfunc('p');
+	printfunc('u');
+	printfunc('t');
+	printfunc('c');
+	printfunc('h');
+	printfunc('a');
+	printfunc('r');
+	putchar('\n');
+	return(0);
+}
+
+
+void printfunc(char a)
+{
+	putchar(a);
+}
