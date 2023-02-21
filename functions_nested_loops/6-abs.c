abf38f25f2375d95457108fa4d5f66fe7850710b
Automatik02 <pro.descombes@gmail.com>
2023-02-21 06:19:24 -0800

    Task 06

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_abs - comptue the absolute value of an integer
+ *@n:takeaint
+ *
+ * Return: Always 0.
+ */
+int _abs(int n)
+{
+
+	unsigned int a = n;
+
+	return (a);
+}
