38465e9b823962e8db9b81f6c86aa547114a166d
Automatik02 <pro.descombes@gmail.com>
2023-02-22 07:02:16 -0800

    Task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *add - return the addition of two numbers
+ *@i: first int
+ *@j: second int
+ *
+ * Return: 0
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -19,3 +19,5 @@ int print_last_digit(int);
 void jack_bauer(void);
 
 void times_table(void);
+
+int add(int i, int j);
