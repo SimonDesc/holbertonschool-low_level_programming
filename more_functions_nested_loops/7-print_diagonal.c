51d7d2586874bc49560d27197cf8e2a233152124
Automatik02 <pro.descombes@gmail.com>
2023-02-23 08:28:47 -0800

    Task 7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -11,18 +11,28 @@ void print_diagonal(int n)
 {
 	int nbre_boucle, nbre_espace;
 
-	for (nbre_boucle = 0; nbre_boucle < n; nbre_boucle++)
+	if (n > 0)
+	{
+		for (nbre_boucle = 0; nbre_boucle < n; nbre_boucle++)
+		{
+			if (nbre_boucle > 0)
+			{
+				for (nbre_espace = 0; nbre_espace < nbre_boucle; nbre_espace++)
+				{
+					_putchar(32);
+				}
+				_putchar('\\');
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar('\\');
+				_putchar('\n');
+			}
+		}
+	}
+	else
 	{
 		_putchar('\n');
-		if (nbre_boucle > 0)
-		{
-			for (nbre_espace = 0; nbre_espace < nbre_boucle; nbre_espace++)
-			{
-				_putchar(32);
-			}
-
-		}
-
-		_putchar('\\');
 	}
 }
