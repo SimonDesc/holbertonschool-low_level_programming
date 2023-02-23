db2c340d4233b70659f0cf5a313eef2e6276c24f
Automatik02 <pro.descombes@gmail.com>
2023-02-23 04:23:32 -0800

    Task 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *more_numbers -Prints 10 times the numbers, from 0 to 14
+ *followed by a new line.
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int j = 0;
+
+	while (j < 10)
+	{
+		int i = 0;
+
+		while (i <= 14)
+		{
+			if (i >= 10)
+			{
+				putchar(i / 10 + '0');
+			}
+			putchar(i % 10 + '0');
+			i++;
+		}
+	j++;
+	putchar('\n');
+	}
+}
