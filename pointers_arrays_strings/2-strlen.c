e610a651ea65bb7cb82bbf41cf1772f0eb925022
Automatik02 <pro.descombes@gmail.com>
2023-02-27 06:43:10 -0800

    Task 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - return the length of a string.
+ * @s: a string
+ *
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+	
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return(i);
+}
