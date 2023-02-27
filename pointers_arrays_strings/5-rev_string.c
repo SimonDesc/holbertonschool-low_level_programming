01ec2227f5851d76d2b8242faa79740fb28d230e
Automatik02 <pro.descombes@gmail.com>
2023-02-27 12:23:19 -0800

    Task 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_rev - print a text in reverse
+ * @s: a pointer str
+ *
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+	int j = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	while (j < i + 1)
+	{
+		if (s[i] == '\0')
+		{
+			i = i - 1;
+		}
+		else
+		{
+			_putchar(s[i]);
+			i = i - 1;
+		}
+	}
+	_putchar('\n');
+	*s = '\0';
+}
