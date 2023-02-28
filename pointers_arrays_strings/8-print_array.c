17605106a5959816bbe8f5df4fc6b1216c91bf41
Automatik02 <pro.descombes@gmail.com>
2023-02-28 07:38:58 -0800

    Task 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_array - return a array of integers
+* @a : the array
+* @n : the integer to stop
+* Return: Always 0
+*/
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (a[i] < a[n])
+	{
+		printf("%d", a[i]);
+		i++;
+
+		if (a[i] != a[n])
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
