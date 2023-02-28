270ad5cf8f7fe95b5c751558c9b696f65029cd8c
Automatik02 <pro.descombes@gmail.com>
2023-02-28 05:53:43 -0800

    Task 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * puts_half - return the second half of the string
+ * @str: the str
+ * Return: Always 0
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+	int j = 0;
+	int max = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	max = i;
+	j = i/2;
+
+	while (j < max)
+	{
+		_putchar(str[j]);
+		j++;
+	}
+
+
+	_putchar('\n');
+}
