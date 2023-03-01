f83d1e14cbaab7e1fb4ca5df94901fc1a5bb6529
Automatik02 <pro.descombes@gmail.com>
2023-03-01 02:14:10 -0800

    Task 09

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_strcpy - copies the string pointed to by src
+ * @dest: the destination
+ * @src: the source
+ * Return: Always 0
+ */
+void *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
