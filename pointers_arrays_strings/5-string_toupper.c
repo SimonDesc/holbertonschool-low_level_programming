4955b9554cfc63a406cf3d295d043e136e4967c3
Automatik02 <pro.descombes@gmail.com>
2023-03-02 03:46:27 -0800

    Task 5

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,17 +1,22 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * string_toupper - Put the caracter in uppercase
  * @s: the string
+ * Return: the char in uppercase
  */
-char *string_toupper(char s*)
+
+char *string_toupper(char *s)
 {
 	int i = 0;
+
 	while (s[i] != '\0')
 	{
-		if (s[i] > 96 || s[i] < 123)
+		if (s[i] > 96 && s[i] < 123)
 		{
 			s[i] = s[i] - ('a' - 'A');
 		}
 		i++;
 	}
+	return (s);
 }
