c7457738f9fb2db51c225df9978d7978c9fbf15d
Automatik02 <pro.descombes@gmail.com>
2023-03-03 02:28:16 -0800

    Task 2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,40 +1,22 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * _memcpy- Writee leing to uppercase
- * @dest: the string in main fonction
- * @src: second parameter
- * @n: the third parameter
+ * _strchr- Writee leing to uppercase
+ * @s: the string in main fonction
+ * @c: the char we must found
  * Return: the string to copy
  */
 char *_strchr(char *s, char c)
 {
-    int i;
-    int k;
-    int j;
-    
-    while (k < s[k])
-    {
-        k++;
-    }
-    
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        if (s[i] == c)
-        {
-            break;
-        }
-    }
-    
-    char n_tab[k];
-    char *p_ntab;*
-    
-    p_ntab = n_tab;
-    
-    for (i; s[i] !='\0'; i++)
-    {
-       n_tab[j] = s[i];
-       j++;
-    }
-    return (p_ntab);
+	unsigned int i;
+
+	for (i = 0; s[i] != '\0' || c == '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return (NULL);
 }
