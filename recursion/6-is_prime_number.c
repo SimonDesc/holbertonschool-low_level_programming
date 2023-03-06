45b177734be743913c9dfee78e4221e0fa21772e
Automatik02 <pro.descombes@gmail.com>
2023-03-06 05:56:35 -0800

    Task 6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,28 +1,52 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+/**
+ * est_premier_recursif - test nombre premier
+ * Vérifie si un nombre est premier de manière récursive en utilisant
+ * une approche de force brute.
+ *
+ * @n: L'entier à tester.
+ * @diviseur: Le diviseur actuellement testé.
+ * Return: true si n est premier, false sinon.
+ */
 bool est_premier_recursif(int n, int diviseur)
 {
-    if (n == 1) {
-        return false;
-    }
-    else if (diviseur * diviseur > n) {
-        return true;
-    }
-    else if (n % diviseur == 0) {
-        return false;
-    }
-    else {
-        return est_premier_recursif(n, diviseur + 1);
-    }
+	if (n == 1)
+	{
+		return (false);
+	}
+	else if (diviseur * diviseur > n)
+	{
+		return (true);
+	}
+	else if (n % diviseur == 0)
+	{
+		return (false);
+	}
+	else
+	{
+		return (est_premier_recursif(n, diviseur + 1));
+	}
 }
 
+/**
+ * is_prime_number - fonction du recoit n
+ * Vérifie si un nombre est premier en appelant la fonction
+ * est_premier_recursif avec le diviseur initial de 2.
+ *
+ * @n: L'entier à tester.
+ * Return: true si n est premier, false sinon.
+ */
 int is_prime_number(int n)
 {
 	if (n < 0)
 	{
 		return (0);
 	}
-    return est_premier_recursif(n, 2);
+	else
+	{
+		return (est_premier_recursif(n, 2));
+	}
 }
 
