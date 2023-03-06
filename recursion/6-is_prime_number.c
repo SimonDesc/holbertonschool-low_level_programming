578dd3488f55489e2de55099dfbc14466b97c735
Automatik02 <pro.descombes@gmail.com>
2023-03-06 05:43:55 -0800

    Task 6

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool est_premier_recursif(int n, int diviseur)
+{
+    if (n == 1) {
+        return false;
+    }
+    else if (diviseur * diviseur > n) {
+        return true;
+    }
+    else if (n % diviseur == 0) {
+        return false;
+    }
+    else {
+        return est_premier_recursif(n, diviseur + 1);
+    }
+}
+
+int is_prime_number(int n)
+{
+    return est_premier_recursif(n, 2);
+}
+
