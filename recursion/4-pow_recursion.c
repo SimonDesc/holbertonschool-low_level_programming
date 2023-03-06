aebefa064681560f98e9291304e6b3fab670dff2
Automatik02 <pro.descombes@gmail.com>
2023-03-06 03:59:05 -0800

    Task 4

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_pow_recursion - returns the value of x raised to the power of y
+ *
+ * @x: the number
+ * @y: the factorial
+ * Return: the factorial of a given number
+ */
+int _pow_recursion(int x, int y)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * (x * y - 1);
+	}
+}
