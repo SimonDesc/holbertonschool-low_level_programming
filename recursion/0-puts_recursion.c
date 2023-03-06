1465f6683228d2d337f5fd3248696edc851419b5
Automatik02 <pro.descombes@gmail.com>
2023-03-06 01:10:59 -0800

    Task 0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * _puts_recursion : print a string, followed by a new line
+ * _puts_recursion: print a string, followed by a new line
  * with recursion
  *
+ * @s:The string to print
  */
 void _puts_recursion(char *s)
 {
-	if ( *s != '\0')
+	if (*s != '\0')
 	{
 		_putchar(*s);
 		s++;
