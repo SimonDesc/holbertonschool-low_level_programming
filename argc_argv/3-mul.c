f5971e4b725faeaf92412195aab6cf2b21bd050b
Automatik02 <pro.descombes@gmail.com>
2023-03-07 03:37:27 -0800

    Task 3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *@argc: the number of arguments
+ *@argv: the arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i = 1;
+	int sum = 0;
+
+	(void) argc;
+	sum = atoi(argv[i]) * atoi(argv[i + 1]);
+	printf("%d\n", sum);
+	return (0);
+}
