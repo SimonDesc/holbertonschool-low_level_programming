f51894815cc8cf8f2216b463263bd3bf167cfa53
Automatik02 <pro.descombes@gmail.com>
2023-03-07 03:44:43 -0800

    Task 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,8 +12,15 @@ int main(int argc, char *argv[])
 	int i = 1;
 	int sum = 0;
 
+	if (argc != 3)
+	{
+		return (1);
+	}
+	else
+	{
 	(void) argc;
 	sum = atoi(argv[i]) * atoi(argv[i + 1]);
 	printf("%d\n", sum);
+	}
 	return (0);
 }
