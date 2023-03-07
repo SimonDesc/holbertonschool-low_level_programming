ce12a83c14f48a7fd72e99d4b7fb4400d4028ae7
Automatik02 <pro.descombes@gmail.com>
2023-03-07 02:32:38 -0800

    Task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *@argc: the number of arguments
+ *@argv: the arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	(void)argv;
+	return (0);
+}
