655989cccf68e613b73d98b3f724449614a6308b
Automatik02 <pro.descombes@gmail.com>
2023-03-13 04:29:59 -0700

    task 2

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *str_concat - concatenates two strings
+ *
+ * @s1: the string 1
+ * @s2: the string 2
+ * Return: NULL on failure
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *ar;
+	int i = 0;
+	int j = 0;
+	int size1 = 0;
+	int size2 = 0;
+	int total = 0;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		size1++;
+	}
+	for (i = 0; s2[i] != '\0'; i++)
+	{
+		size2++;
+	}
+	total = size1 + size2;
+
+	ar = malloc(total * sizeof(char) + 1);
+	if (ar == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size1; i++)
+	{
+		ar[i] = s1[i];
+	}
+
+	for (i = size1; i < (total); i++, j++)
+	{
+		ar[i] = s2[j];
+	}
+
+	return (ar);
+
+}
