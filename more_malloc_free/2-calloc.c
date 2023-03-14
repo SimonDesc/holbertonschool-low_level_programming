e16cda6d9803f7a1d18881fccfcc7acc36d9d5a0
Automatik02 <pro.descombes@gmail.com>
2023-03-14 09:49:38 -0700

    Task 2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _calloc - a malloc ++
+ *
+ * @nmemb: the array of int
+ * @size: the size
+ * Return: return the array
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p = NULL;
+	int i = 0;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	p = malloc(nmemb * size);
+	
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < (nmemb * size) ; i++)
+	{
+		p[i] = 0;
+	}
+	return (p);
+}
