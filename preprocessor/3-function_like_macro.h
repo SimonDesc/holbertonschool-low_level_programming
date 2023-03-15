ea410e69bbc763ed7b0c1665fca5f1cf3fcc6848
Automatik02 <pro.descombes@gmail.com>
2023-03-15 13:02:44 -0700

    Task 3

--- preprocessor/3-function_like_macro.h
+++ preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
 #ifndef _3_FUNCTION_LIKE_MACRO_
 #define _3_FUNCTION_LIKE_MACRO_
-#define ABS(X) ((X) < (0) ? (-X) : (X))
+#define ABS(X) ((X) < (0) ? -(X) : (X))
 #endif /* _3_FUNCTION_LIKE_MACRO_ */
