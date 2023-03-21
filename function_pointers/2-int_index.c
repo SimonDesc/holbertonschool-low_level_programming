be590fb2b9d21497ed6972e57ba0cabf4d3210b2
Automatik02 <pro.descombes@gmail.com>
2023-03-21 04:46:22 -0700

    Task 2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * array_iterator - iterate a array
+ *
+ *
+ *@array: the array
+ *@size: the size
+ *@action: function pointer
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned long int i = 0;
+
+	if (array == NULL || size == NULL)
+	{
+		return;
+	}
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	for (; i < size; i++)
+	{
+		if (cmp(array[i] != 0))
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
