06e3051482dd71f174c104db7c5cb3710a702183
Automatik02 <pro.descombes@gmail.com>
2023-04-06 02:40:35 -0700

    Task 8

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,52 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - Short description, single line
+ * @head: the struct
+ * @index: index
+ (*
+ * Return: The total of the nodes
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int pos = 0;
+
+	dlistint_t *tmp = *head;
+	dlistint_t *tmp2 = NULL;
+
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+
+	if ((*head)->next == NULL)
+	{
+		free(*head);
+		*head = NULL;
+		return (1);
+	}
+
+	if (index == 0)
+	{
+		tmp->next->prev = NULL;
+		*head = tmp->next;
+		free(tmp);
+		return (1);
+	}
+	while (tmp != NULL && pos != index)
+	{
+		tmp2 = tmp;
+		tmp = tmp->next;
+		pos++;
+	}
+
+	if (tmp == NULL)
+	{
+		return (-1);
+	}
+
+	tmp2->next = tmp->next;
+	tmp->next->prev = tmp2;
+	free(tmp);
+
+	return (1);
+}
