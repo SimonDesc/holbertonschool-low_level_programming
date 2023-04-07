a6a76913ef0f4616788a601b92118f72e337a8c2
Automatik02 <pro.descombes@gmail.com>
2023-04-07 02:01:55 -0700

    First commit

---
+++ bit_manipulation/main.h
@@ -0,0 +1,10 @@
+#ifndef bits
+#define bits
+#include <stdio.h>
+#include <string.h>
+
+unsigned int binary_to_uint(const char *b);
+int _pow_recursion(int x, int y);
+void print_binary(unsigned long int n);
+int _putchar(char c);
+#endif
