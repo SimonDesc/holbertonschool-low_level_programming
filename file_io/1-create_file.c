7718bde34a3a3cd9604010ca6c5bcc782ffe93a7
Automatik02 <pro.descombes@gmail.com>
2023-04-11 03:39:52 -0700

    First commit function that creates a file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * create_file- Function that creates a file
+ *
+ * @filename: the name of the file
+ * @text_content: the text to integrate
+ *
+ * Return: 1 on sucess, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	fd = open(filename, O_RDWR | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	write(fd, text_content, strlen(text_content));
+	close(fd);
+
+	return (1);
+}
