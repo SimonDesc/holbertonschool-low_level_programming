80c9e215eaea0bbe548e5561284cc49923b0720b
Automatik02 <pro.descombes@gmail.com>
2023-04-11 03:44:55 -0700

    Fix condition to check if text is NULL

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -11,9 +11,13 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 	{
 		return (0);
+	}
+	if (text_content == NULL)
+	{
+		return (1);
 	}
 
 	fd = open(filename, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
