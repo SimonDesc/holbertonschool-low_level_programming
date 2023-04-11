c24ef2b692e9fd051797ebbc2a88e6b74c87de6e
Automatik02 <pro.descombes@gmail.com>
2023-04-11 03:42:16 -0700

    Fix to not append text

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 		return (0);
 	}
 
-	fd = open(filename, O_RDWR | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 	{
 		return (-1);
