4d9d8f5da20aaf841240dad5060501455040b247
Automatik02 <pro.descombes@gmail.com>
2023-04-11 02:49:11 -0700

    First commit

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * read_textfile- Function that reads a text file and
+ * prints it to the POSIX standard output
+ *
+ * @filename: the name of the file
+ * @letters: the number of letters it should read and print
+ *
+ * Return: The actual number of letter
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buffer;
+	ssize_t rcount;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		return (0);
+	}
+
+	rcount = read(fd, buffer, letters);
+	if (recount == -1)
+	{
+		return (0);
+	}
+	write(1, buffer, rcount);
+	return (rcount);
+}
---
+++ file_io/main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_
+#define _MAIN_
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
